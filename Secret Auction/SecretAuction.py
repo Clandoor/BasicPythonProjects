18ae5617bc4385428d4cf152904dd1b967a2cbef
Siddharth Joshi <siddharthjoshi329@gmail.com>
2023-04-17 16:25:44 -0400

    Create SecretAuction.py

---
+++ Secret Auction/SecretAuction.py
@@ -0,0 +1,35 @@
+"""Following program works in a scenario where people are bidding via a machine and where they can't compete for bids. Each bid is kept unknown from the previous and other bidders.
+At the end, the bidder with the highest bid wins hence revealing their name and an appropriate messsage.
+"""
+
+import os
+from art import logo
+
+
+def findHighestBidder(bids):
+    highestBid = 0
+    winnersName = ""
+    for biddersName in bids:
+        if bids[biddersName] > highestBid:
+            highestBid = bids[biddersName]
+            winnersName = biddersName
+
+    print(f"The winner is {winnersName} with a bid of ${highestBid}.")
+
+
+print(logo)
+bids = {}
+
+continueBidding = True
+
+while continueBidding:
+    biddersName = input("Enter your name here: ")
+    biddersAmount = int(input("Enter your bid here: $"))
+    bids[biddersName] = biddersAmount
+    moreBidders = input("Are there any more bidders? (Yes / No): ").lower()
+    os.system('clear')
+
+    if moreBidders == "no":
+        continueBidding = False
+
+findHighestBidder(bids)
